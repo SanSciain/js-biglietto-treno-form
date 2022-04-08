410722a3b4a793fb391bcf0593c3c541a249ae2a
SanSciain <mastroeni.santi@gmail.com>
2022-04-08 16:44:41 +0200

    Add: alert when wrong input, box shadow

--- js/script.js
+++ js/script.js
@@ -12,36 +12,37 @@ btnGenerate.addEventListener("click",
 
         const userAge = document.getElementById("user-age").value;
 
-        console.log(userName);
-        console.log(userTrip, typeof(userTrip));
+        if ((userName !== "") && (userTrip != NaN) && (userAge !== "")) {
+            let priceTicket = userTrip * perKm;
 
-        let priceTicket = userTrip * perKm;
+            let offer;
 
-        let offer;
+            if (userAge === "minor") {
+                priceTicket = (priceTicket - priceTicket * minorDiscount).toFixed(2);
+                offer = "Minorenne";
+            } else if (userAge === "adult") {
+                priceTicket = (priceTicket).toFixed(2);
+                offer = "Standard"
+            } else if (userAge === "over") {
+                priceTicket = (priceTicket - priceTicket * overDiscont).toFixed(2);
+                offer = "Over 65";
+            }
 
-        if (userAge === "minor") {
-            priceTicket = (priceTicket - priceTicket * minorDiscount).toFixed(2);
-            offer = "Minorenne";
-        } else if (userAge === "adult") {
-            priceTicket = (priceTicket).toFixed(2);
-            offer = "Standard"
-        } else if (userAge === "over") {
-            priceTicket = (priceTicket - priceTicket * overDiscont).toFixed(2);
-            offer = "Over 65";
+            document.querySelector(".passenger p").innerHTML = userName;
+
+            document.querySelector(".offer p").innerHTML = offer;
+
+
+            document.querySelector(".wagon p").innerHTML = Math.floor(Math.random() * (9 - 1 + 1) ) + 1;;
+
+            document.querySelector(".codecp p").innerHTML = Math.floor(Math.random() * (999999 - 900000 + 1) ) + 900000;;
+
+            document.querySelector(".price p").innerHTML = priceTicket;
+
+            document.getElementById("hidden").classList.remove("d-none")
+        } else {
+            alert("Inserire i Dati corettamente");
         }
-
-        document.querySelector(".passenger p").innerHTML = userName;
-
-        document.querySelector(".price p").innerHTML = priceTicket;
-
-        document.querySelector(".wagon p").innerHTML = Math.floor(Math.random() * (9 - 1 + 1) ) + 1;;
-
-        document.querySelector(".codecp p").innerHTML = Math.floor(Math.random() * (999999 - 900000 + 1) ) + 900000;;
-
-        document.querySelector(".offer p").innerHTML = offer;
-
-        document.getElementById("hidden").classList.remove("d-none")
-
     }
 )
 
